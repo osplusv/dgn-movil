a69fc58a27be5f06202f4a73d052ade4749280f3
lgaleana <lsgaleana@gmail.com>
2014-10-18 14:01:53 -0500

    Implemented tabs in home screen

--- app/src/main/java/com/oracle/dgnmovil/app/MainActivity.java
+++ app/src/main/java/com/oracle/dgnmovil/app/MainActivity.java
@@ -1,7 +1,12 @@
 package com.oracle.dgnmovil.app;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,23 +15,64 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-
+import com.oracle.dgnmovil.tabs.FavoritesTabFragment;
 
 public class MainActivity extends ActionBarActivity {
+
+    final String[] TABS = { "Relevantes", "Reportados" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new ApplicateFragmentAdapter(getSupportFragmentManager()));
+
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // hide the given tab
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // probably ignore this event
+            }
+        };
+
+        actionBar.addTab(actionBar.newTab()
+                        .setText("Favoritos")
+                        .setTabListener(tabListener));
+
+        actionBar.addTab(actionBar.newTab()
+                .setText("Relevantes")
+                .setTabListener(tabListener));
+
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                getActionBar().setSelectedNavigationItem(position);
+            }
+        });
+
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
+        }*/
     }
 
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -43,6 +89,27 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }*/
+
+    private class ApplicateFragmentAdapter extends FragmentPagerAdapter {
+        public ApplicateFragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new FavoritesTabFragment();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TABS[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TABS.length;
+        }
     }
 
     /**
