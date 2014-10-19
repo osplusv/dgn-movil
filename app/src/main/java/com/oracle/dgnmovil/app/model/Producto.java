5c2c341cc70d2dd910dc7e206ca3b2e7c7d64d7e
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2014-10-18 19:49:34 -0500

    Search implemented

---
+++ app/src/main/java/com/oracle/dgnmovil/app/model/Producto.java
@@ -0,0 +1,36 @@
+package com.oracle.dgnmovil.app.model;
+
+import java.util.List;
+
+/**
+ * Created by osvaldo on 10/18/14.
+ */
+public class Producto {
+    private String nombre;
+    private long numNormas;
+    private List<String> img;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getNumNormas() {
+        return numNormas;
+    }
+
+    public void setNumNormas(long numNormas) {
+        this.numNormas = numNormas;
+    }
+
+    public List<String> getImg() {
+        return img;
+    }
+
+    public void setImg(List<String> img) {
+        this.img = img;
+    }
+}
---
+++ app/src/main/java/com/oracle/dgnmovil/app/model/Rae.java
@@ -0,0 +1,34 @@
+package com.oracle.dgnmovil.app.model;
+
+/**
+ * Created by osvaldo on 10/18/14.
+ */
+public class Rae {
+    private String nombre;
+    private String img;
+    private long numNormas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public long getNumNormas() {
+        return numNormas;
+    }
+
+    public void setNumNormas(long numNormas) {
+        this.numNormas = numNormas;
+    }
+}
