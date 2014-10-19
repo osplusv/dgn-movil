282aea6d704b823b12d07c2419b3fef1db29a452
Osvaldo Villagrana <osvaldo.villagrana@oracle.com>
2014-10-19 04:49:22 -0500

    Favorites fragment, Report fragmet and Report activity

--- app/src/main/java/com/oracle/dgnmovil/app/ReportActivity.java
+++ app/src/main/java/com/oracle/dgnmovil/app/ReportActivity.java
@@ -2,22 +2,23 @@ package com.oracle.dgnmovil.app;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
-import android.os.AsyncTask;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.oracle.dgnmovil.util.MultipartUtility;
+import com.firebase.client.Firebase;
+import com.firebase.client.utilities.Base64;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ReportActivity extends ActionBarActivity {
@@ -26,11 +27,30 @@ public class ReportActivity extends ActionBarActivity {
 
     ImageView photo;
     Bitmap imageBitmap;
+    String mImage;
+    Firebase mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mFirebaseRef = new Firebase("https://dgn.firebaseio.com/");
+
         setContentView(R.layout.activity_report);
+
+        int titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        TextView titleTextView = (TextView) findViewById(titleId);
+        titleTextView.setTextColor(Color.WHITE);
+        titleTextView.setTypeface(Typeface.createFromAsset(getAssets(), "font/MavenPro-Bold.ttf"));
+
+        TextView informacion = (TextView) findViewById(R.id.informacion);
+        informacion.setTypeface(Typeface.createFromAsset(getAssets(), "font/MavenPro-Bold.ttf"));
+
+        TextView comentarios = (TextView) findViewById(R.id.comentarios);
+        comentarios.setTypeface(Typeface.createFromAsset(getAssets(), "font/MavenPro-Bold.ttf"));
+
+        TextView fotografia = (TextView) findViewById(R.id.fotografia);
+        fotografia.setTypeface(Typeface.createFromAsset(getAssets(), "font/MavenPro-Bold.ttf"));
 
         Button button_camera = (Button) findViewById(R.id.button_camera);
         photo = (ImageView) findViewById(R.id.image_photo);
@@ -42,6 +62,37 @@ public class ReportActivity extends ActionBarActivity {
                 }
             }
         });
+
+        Button enviar = (Button) findViewById(R.id.send_report);
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Firebase postRef = mFirebaseRef.child("reportes");
+                Map<String, String> post = new HashMap<String, String>();
+
+                EditText producto_text = (EditText) findViewById(R.id.producto_textbox);
+                String producto = producto_text.getText().toString();
+
+                post.put("producto", producto);
+
+                EditText ubicacion_text = (EditText) findViewById(R.id.ubicacion_textbox);
+                String ubicacion = ubicacion_text.getText().toString();
+
+                post.put("ubicacion", ubicacion);
+
+                EditText comentario_text = (EditText) findViewById(R.id.comentarios_textbox);
+                String comentarios = ubicacion_text.getText().toString();
+
+                post.put("comentario", comentarios);
+
+                if (mImage != null) {
+                    post.put("imagen", mImage);
+                }
+
+                postRef.push().setValue(post);
+            }
+        });
+
     }
 
     @Override
@@ -50,50 +101,16 @@ public class ReportActivity extends ActionBarActivity {
             Bundle extras = data.getExtras();
             imageBitmap = (Bitmap) extras.get("data");
             photo.setImageBitmap(imageBitmap);
-            new SendHttpRequestTask().execute("monkey", "monkey2");
+            mImage = imageToBase64(imageBitmap);
+            // new SendHttpRequestTask().execute("monkey", "monkey2");
         }
     }
 
-    private class SendHttpRequestTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            String charset = "UTF-8";
-            String requestURL = "http://ec2-54-69-165-46.us-west-2.compute.amazonaws.com:8080/applicate/sayhello";
-
-            try {
-                MultipartUtility multipart = new MultipartUtility(requestURL, charset);
-
-                multipart.addFormField("firstName", params[0]);
-                multipart.addFormField("lastName", params[1]);
-
-                File f = new File(ReportActivity.this.getCacheDir(), "tempFile");
-                f.createNewFile();
-
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                imageBitmap.compress(CompressFormat.PNG, 0, bos);
-                byte[] bitmapdata = bos.toByteArray();
-
-                FileOutputStream fos = new FileOutputStream(f);
-                fos.write(bitmapdata);
-
-                fos.flush();
-                fos.close();
-
-                multipart.addFilePart("photo", f);
-
-                List<String> response = multipart.finish();
-
-                System.out.println("SERVER REPLIED:");
-
-                for (String line : response) {
-                    System.out.println(line);
-                }
-            } catch (IOException ex) {
-                System.err.println(ex);
-            }
-
-            return null;
-        }
+    private String imageToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] b = baos.toByteArray();
+        return Base64.encodeBytes(b);
     }
+
 }
